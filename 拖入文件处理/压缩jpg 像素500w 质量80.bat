4fab4480a988fc42b4b2f2e29b1abece04fc86f7
Haujet <1292756898@qq.com>
2021-06-18 21:59:49 +0800

    增加了 jpeg 压缩的处理

--- 拖入文件处理/压缩jpg 像素500w 质量80.bat
+++ 拖入文件处理/压缩jpg 像素500w 质量80.bat
@@ -1,12 +1,25 @@
+setlocal enabledelayedexpansion
 @echo off
 
 :: 作者：淳帅二代
 :: 日期：2021 年 6 月 18 日
 :: 运行此脚本需要先确保安装有：ImageMagick
+:: 这个脚本会把拖入的像素数大于 500w 的 jpg 和 jpeg 图片
+:: 缩小分辨率到 500w 像素，并且按质量 80 压缩。
 
 for %%i in (%*) do (
+	
 	if /i %%~xi==.jpg (
-	    magick %%i -resize "5000000@" -quality 80 %%i
+		set 是否处理=yes
+	) else if /i %%~xi==.jpeg (
+		set 是否处理=yes
+	) else (
+		set 是否处理=noo
+	)
+	
+	if /i !是否处理!==yes (
+		echo 正在处理 %%i
+		magick %%i -resize "5000000@>" -quality 80 %%i
 	)
 )
 
